068d7391757bf17b1099ee1e4fc3a205419a76f2
Cooper Sawyer <cooper.sawyer@powerhrg.com>
2021-08-20 11:34:49 -0400

    commit tree 41645a52fb0ca1e61f38364f7408d803d6ebd903

--- lib/answer.rb
+++ lib/answer.rb
@@ -1,3 +1,12 @@
 class Answer
+  attr_reader :content, :votes, :question_id
+  attr_accessor :id
+
+  def intialize(content:, votes:, question_id:, id: nil)
+    @content = content
+    @votes = votes
+    @id = id
+    @question_id = question_id
+  end
 
 end
